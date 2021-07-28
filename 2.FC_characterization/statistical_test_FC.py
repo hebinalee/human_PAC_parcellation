f7c91e3d1610a0f73d7ced7f0c8eb3eb6ea2e86b
Hebina Lee <62586511+hebinalee@users.noreply.github.com>
2021-07-29 00:40:14 +0900

    Update and rename d_stat.py to statistical_test_FC.py

--- 2.FC_characterization/statistical_test_FC.py
+++ 2.FC_characterization/statistical_test_FC.py
@@ -1,6 +1,6 @@
-######################################
-## functional connectivity analysis ##
-######################################
+######################
+## STATISTICAL TEST ##
+######################
 import os
 from os import listdir
 from os.path import join, exists, isfile, isdir
