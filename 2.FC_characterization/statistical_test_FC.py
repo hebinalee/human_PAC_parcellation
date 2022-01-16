e1887cc04a51d48bb5d4982df4f473207604fd17
Hebina Lee <62586511+hebinalee@users.noreply.github.com>
2022-01-16 22:16:29 +0900

    Update statistical_test_FC.py

--- 2.FC_characterization/statistical_test_FC.py
+++ 2.FC_characterization/statistical_test_FC.py
@@ -5,53 +5,74 @@
 ## ttest   : To perform two-sample t-test for each pair of ROIs
 ##           and correct multiple comparison problem 
 #################################################################
-
+'''
+[Order of function implementation]
+1) roiconn
+2) ttest
+'''
 import os
 from os import listdir
 from os.path import join, exists, isfile, isdir
+import shutil
 import numpy as np
-from scipy.stats import ttest_ind
-from scipy.stats import f_oneway
-from statsmodels.stats.multitest import multipletests
-import pandas as pd
+import nibabel as nib
+import matplotlib.pyplot as plt
 
-basepath = 'X:/path/myfolder/'
+if os.name == 'nt':
+	store7 = 'V:/'
+else:
+	store7 = '/store7/'
+
 Nclusters = 3
 
-def roiconn(hemi, conntype):
-	datapath = basepath + 'data/'
-	sublist = listdir(datapath)
+
+'''
+[roiconn]
+To save matrix of meanFC values for each ROI (N_valid_subj X N_clusters)
+
+Input:  /store7/hblee/MPI/data1/{subID}/cluster4/relabel-SC/meanFC.{hemi}.K3.npy (3 X 82)
+Output: /store7/hblee/MPI/stat/roiFC/{hemi}-ROI{i}.npy
+'''
+def roiconn(hemi):
+	datapath = store7 + 'hblee/MPI/data/'
+	sublist = sorted(listdir(datapath))
 	Nsub = len(sublist)
-	Nroi = 41
-	conn = ['SC', 'FC']
-	connname = conn[conntype-1]
+	Nroi = 82
 
 	meanconn = np.zeros((Nsub, Nclusters, Nroi))
 	i = 0
-	for sidx, sname in enumerate(sublist):
-		subpath = join(basepath, 'data', sname, 'cluster/relabel-SC/')
+	for sidx, subID in enumerate(sublist):
+		subpath = f'{store7}hblee/MPI/data/{subID}/cluster'
 
-		if exists(subpath + 'mean%s.%s.K%d.npy' %(connname, hemi, Nclusters)):
-			subconn = np.load(subpath + 'mean%s.%s.K%d.npy' %(connname, hemi, Nclusters))
+		if exists(f'{subpath}/meanFC.{hemi}.K{Nclusters}.npy'):
+			subconn = np.load(f'{subpath}/meanFC.{hemi}.K{Nclusters}.npy')
 			meanconn[i, :, :] = subconn
 			i = i + 1
 
 	for roi in range(Nroi):
 		roiconn = meanconn[:,:,roi]
-		np.save(f'{basepath}/stat/cluster/roi{connname}/{hemi}-ROI{roi+1}.npy', roiconn)
+		np.save(f'{store7}hblee/MPI/stat/roiFC/{hemi}-ROI{roi+1}.npy', roiconn)
 
 
-def ttest(hemi, conntype):
-	Nroi = 41
-	inpath = join(basepath, 'stat/cluster')
-	conn = ['SC', 'FC']
-	connname = conn[conntype-1]
+'''
+[ttest]
+To perform two-sample t-test for each pair of data (FDR correction is applied)
+
+Input:  /store7/hblee/MPI/stat/roiFC/{hemi}-ROI{i}.npy
+Output: 1) /store7/hblee/MPI/stat/{hemi}-FC-t_statistics.npy
+		2) /store7/hblee/MPI/stat/{hemi}-FC-t_pvalues.npy
+'''
+from scipy.stats import ttest_ind
+from statsmodels.stats.multitest import multipletests
+def ttest(hemi):
+	Nroi = 82
+	inpath = f'{store7}hblee/MPI/stat'
 
 	t = np.zeros((Nroi, Nclusters))
 	p = np.zeros((Nroi, Nclusters))
 	corrected = np.zeros_like(p)
 	for roi in range(Nroi):
-		conn = np.load(f'{inpath}/roi{connname}/{hemi}-ROI{roi+1}.npy')
+		conn = np.load(f'{inpath}/roiFC/{hemi}-ROI{roi+1}.npy')
 		t[roi,0], p[roi,0] = ttest_ind(conn[:,0], conn[:,1])
 		t[roi,1], p[roi,1] = ttest_ind(conn[:,1], conn[:,2])
 		t[roi,2], p[roi,2] = ttest_ind(conn[:,2], conn[:,0])
@@ -60,9 +81,38 @@ def ttest(hemi, conntype):
 		_, p_corr, _, _ = multipletests(p[:, i], 0.05, 'fdr_bh')
 		corrected[:, i] = p_corr
 
-	np.save(f'{inpath}/{hemi}-{connname}-t_statistics.npy', t)
-	np.save(f'{inpath}/{hemi}-{connname}-t_pvalues.npy', corrected)
+	np.save(f'{inpath}/{hemi}-FC-t_statistics.npy', t)
+	np.save(f'{inpath}/{hemi}-FC-t_pvalues.npy', corrected)
 	
 	df = pd.DataFrame(np.hstack((t, p, corrected)))
-	filepath = inpath + '%s_stat_results.xlsx' %hemi
+	filepath = if'{inpath}/{hemi}_stat_results.xlsx'
 	df.to_excel(filepath, index=False)
+
+
+'''
+[main]
+Main function to perform analysis
+'''
+def main(a, b, startname=None):
+	datapath = store4 + 'hblee/4.MPI/4.clustFC/data/'
+	sublist = listdir(datapath)
+	if startname:
+		a = sublist.index(startname)
+	
+	if b==0:
+		sublist = sublist[a:]
+	else:
+		sublist = sublist[a:b]  # 162026 ~ 793465
+	
+	# 1. roiconn(hemi)
+	# 2. ttest(hemi)
+
+
+if __name__ == "__main__":
+	import argparse
+	parser = argparse.ArgumentParser(description="functional_connectivity")
+	parser.add_argument(dest="startpoint",type=int,help="Start point of subject for data processing")
+	parser.add_argument(dest="endpoint",type=int,help="End point of subject for data processing")
+	parser.add_argument("-s",dest="startname",help="The name of the subject to start",required=False)
+	args=parser.parse_args()
+	main(args.startpoint, args.endpoint, args.verison, args.startname)
