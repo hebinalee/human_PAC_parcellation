b92379cbdd66851db7869e8f74528ebac61275a5
Hebina Lee <62586511+hebinalee@users.noreply.github.com>
2022-01-16 22:17:02 +0900

    Update compute_meanFC.py

--- 2.FC_characterization/compute_meanFC.py
+++ 2.FC_characterization/compute_meanFC.py
@@ -95,68 +95,6 @@ def compute_meanFC(subID):
 
 		np.save(f'{outpath}/meanFC.{hemi}.K{Nclusters}.npy', meanFC)
 
-'''
-[roiconn]
-To save matrix of meanFC values for each ROI (N_valid_subj X N_clusters)
-
-Input:  /store7/hblee/MPI/data1/{subID}/cluster4/relabel-SC/meanFC.{hemi}.K3.npy (3 X 82)
-Output: /store7/hblee/MPI/stat/roiFC/{hemi}-ROI{i}.npy
-'''
-def roiconn(hemi):
-	datapath = store7 + 'hblee/MPI/data/'
-	sublist = sorted(listdir(datapath))
-	Nsub = len(sublist)
-	Nroi = 82
-
-	meanconn = np.zeros((Nsub, Nclusters, Nroi))
-	i = 0
-	for sidx, subID in enumerate(sublist):
-		subpath = f'{store7}hblee/MPI/data/{subID}/cluster'
-
-		if exists(f'{subpath}/meanFC.{hemi}.K{Nclusters}.npy'):
-			subconn = np.load(f'{subpath}/meanFC.{hemi}.K{Nclusters}.npy')
-			meanconn[i, :, :] = subconn
-			i = i + 1
-
-	for roi in range(Nroi):
-		roiconn = meanconn[:,:,roi]
-		np.save(f'{store7}hblee/MPI/stat/roiFC/{hemi}-ROI{roi+1}.npy', roiconn)
-
-
-'''
-[ttest]
-To perform two-sample t-test for each pair of data (FDR correction is applied)
-
-Input:  /store7/hblee/MPI/stat/roiFC/{hemi}-ROI{i}.npy
-Output: 1) /store7/hblee/MPI/stat/{hemi}-FC-t_statistics.npy
-		2) /store7/hblee/MPI/stat/{hemi}-FC-t_pvalues.npy
-'''
-from scipy.stats import ttest_ind
-from statsmodels.stats.multitest import multipletests
-def ttest(hemi):
-	Nroi = 82
-	inpath = f'{store7}hblee/MPI/stat'
-
-	t = np.zeros((Nroi, Nclusters))
-	p = np.zeros((Nroi, Nclusters))
-	corrected = np.zeros_like(p)
-	for roi in range(Nroi):
-		conn = np.load(f'{inpath}/roiFC/{hemi}-ROI{roi+1}.npy')
-		t[roi,0], p[roi,0] = ttest_ind(conn[:,0], conn[:,1])
-		t[roi,1], p[roi,1] = ttest_ind(conn[:,1], conn[:,2])
-		t[roi,2], p[roi,2] = ttest_ind(conn[:,2], conn[:,0])
-
-	for i in range(Nclusters):
-		_, p_corr, _, _ = multipletests(p[:, i], 0.05, 'fdr_bh')
-		corrected[:, i] = p_corr
-
-	np.save(f'{inpath}/{hemi}-FC-t_statistics.npy', t)
-	np.save(f'{inpath}/{hemi}-FC-t_pvalues.npy', corrected)
-	
-	df = pd.DataFrame(np.hstack((t, p, corrected)))
-	filepath = if'{inpath}/{hemi}_stat_results.xlsx'
-	df.to_excel(filepath, index=False)
-
 
 '''
 [main]
@@ -173,8 +111,6 @@ def main(a, b, startname=None):
 	else:
 		sublist = sublist[a:b]  # 162026 ~ 793465
 	
-	# 2. roiconn(hemi)
-	# 3. ttest(hemi)
 	for sidx, subID in enumerate(sublist):
 		print('%dth sub - %s - FC analysis on cluster\n' %(sidx+1, subID))
 		#1. compute_meanFC(subID)
