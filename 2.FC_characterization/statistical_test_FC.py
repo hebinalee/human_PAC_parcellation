df47645bca1630f335ec2f9478b74a3fdee09d71
Hebina Lee <62586511+hebinalee@users.noreply.github.com>
2022-02-28 11:50:33 +0900

    Update statistical_test_FC.py

--- 2.FC_characterization/statistical_test_FC.py
+++ 2.FC_characterization/statistical_test_FC.py
@@ -18,10 +18,10 @@ import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
 
-basepath = 'X:/path/myfolder'
-datapath = basepath + '/data'
+BASE_DIR = 'X:/path/myfolder'
+DATA_DIR = BASE_DIR + '/data'
 
-def set_subpath(subID): return f'{datapath}/{subID}'
+def set_subjdir(subID): return f'{DATA_DIR}/{subID}'
 
 Nclusters = 3
 
@@ -31,48 +31,48 @@ Nclusters = 3
 [roiconn]
 To save matrix of meanFC values for each ROI (N_valid_subj X N_clusters)
 
-Input:  {subpath}/cluster/relabel/meanFC.{hemi}.K3.npy (3 X 82)
-Output: {basepath}/stat/roiFC/{hemi}-ROI{i}.npy
+Input:  {subj_dir}/cluster/relabel/meanFC.{hemi}.K3.npy (3 X 82)
+Output: {BASE_DIR}/stat/roiFC/{hemi}-ROI{i}.npy
 '''
 def roiconn(hemi):
-	sublist = sorted(listdir(datapath))
+	sublist = sorted(listdir(DATA_DIR))
 	Nsub = len(sublist)
 	Nroi = 82
 
 	meanconn = np.zeros((Nsub, Nclusters, Nroi))
 	i = 0
 	for sidx, subID in enumerate(sublist):
-		subpath = set_subpath(subID) + '/cluster'
+		subj_dir = set_subjdir(subID) + '/cluster'
 
-		if exists(f'{subpath}/relabel/meanFC.{hemi}.K{Nclusters}.npy'):
-			subconn = np.load(f'{subpath}/relabel/meanFC.{hemi}.K{Nclusters}.npy')
+		if exists(f'{subj_dir}/relabel/meanFC.{hemi}.K{Nclusters}.npy'):
+			subconn = np.load(f'{subj_dir}/relabel/meanFC.{hemi}.K{Nclusters}.npy')
 			meanconn[i, :, :] = subconn
 			i = i + 1
 
 	for roi in range(Nroi):
 		roiconn = meanconn[:,:,roi]
-		np.save(f'{basepath}/stat/roiFC/{hemi}-ROI{roi+1}.npy', roiconn)
+		np.save(f'{BASE_DIR}/stat/roiFC/{hemi}-ROI{roi+1}.npy', roiconn)
 
 
 '''
 [ttest]
 To perform two-sample t-test for each pair of data (FDR correction is applied)
 
-Input:  {basepath}/stat/roiFC/{hemi}-ROI{i}.npy
-Output: 1) {basepath}/stat/{hemi}-FC-t_statistics.npy
-	2) {basepath}/stat/{hemi}-FC-t_pvalues.npy
+Input:  {BASE_DIR}/stat/roiFC/{hemi}-ROI{i}.npy
+Output: 1) {BASE_DIR}/stat/{hemi}-FC-t_statistics.npy
+	2) {BASE_DIR}/stat/{hemi}-FC-t_pvalues.npy
 '''
 from scipy.stats import ttest_ind
 from statsmodels.stats.multitest import multipletests
 def ttest(hemi):
 	Nroi = 82
-	inpath = f'{basepath}/stat'
+	input_dir = f'{BASE_DIR}/stat'
 
 	t = np.zeros((Nroi, Nclusters))
 	p = np.zeros((Nroi, Nclusters))
 	corrected = np.zeros_like(p)
 	for roi in range(Nroi):
-		conn = np.load(f'{inpath}/roiFC/{hemi}-ROI{roi+1}.npy')
+		conn = np.load(f'{input_dir}/roiFC/{hemi}-ROI{roi+1}.npy')
 		t[roi,0], p[roi,0] = ttest_ind(conn[:,0], conn[:,1])
 		t[roi,1], p[roi,1] = ttest_ind(conn[:,1], conn[:,2])
 		t[roi,2], p[roi,2] = ttest_ind(conn[:,2], conn[:,0])
@@ -81,11 +81,11 @@ def ttest(hemi):
 		_, p_corr, _, _ = multipletests(p[:, i], 0.05, 'fdr_bh')
 		corrected[:, i] = p_corr
 
-	np.save(f'{inpath}/{hemi}-FC-t_statistics.npy', t)
-	np.save(f'{inpath}/{hemi}-FC-t_pvalues.npy', corrected)
+	np.save(f'{input_dir}/{hemi}-FC-t_statistics.npy', t)
+	np.save(f'{input_dir}/{hemi}-FC-t_pvalues.npy', corrected)
 	
 	df = pd.DataFrame(np.hstack((t, p, corrected)))
-	filepath = if'{inpath}/{hemi}_stat_results.xlsx'
+	filepath = if'{input_dir}/{hemi}_stat_results.xlsx'
 	df.to_excel(filepath, index=False)
 
 
@@ -94,7 +94,7 @@ def ttest(hemi):
 Main function to perform analysis
 '''
 def main(a, b, startname=None):
-	sublist = listdir(datapath)
+	sublist = listdir(DATA_DIR)
 	if startname:
 		a = sublist.index(startname)
 	
